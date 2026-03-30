5ba3e95ac338a08813c193cab402efdab23b9418
bullalucas756-glitch <bullalucas756@gmail.com>
2026-03-30 14:45:39 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import routes from './routes.js';
+
+export const store = Vue.reactive({
+    dark: true,
+    toggleDark() {
+    },
+});
+
+
+const app = Vue.createApp({
+    data: () => ({ store }),
+});
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+app.use(router);
+
+app.mount('#app');
